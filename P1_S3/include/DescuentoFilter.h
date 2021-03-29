74dba9c140d569b9b8a5ca2ce680b75a717186aa
pablojj1808 <pablojjinfo@gmail.com>
2021-03-29 15:49:28 +0200

    MODIFICO ESTRUCTURA DE LA CARPETA

---
+++ P1_S3/include/DescuentoFilter.h
@@ -0,0 +1,14 @@
+//
+// Created by Pablo Jj on 29/3/21.
+//
+
+#ifndef P1_S3_DESCUENTOFILTER_H
+#define P1_S3_DESCUENTOFILTER_H
+
+
+class DescuentoFilter {
+
+};
+
+
+#endif //P1_S3_DESCUENTOFILTER_H
---
+++ P1_S3/src/DescuentoFilter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pablo Jj on 29/3/21.
+//
+
+#include "DescuentoFilter.h"
